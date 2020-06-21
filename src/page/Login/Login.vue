5fe891dee7b067355b6b2c91e09cd61f900c306e
haibin-gua <1305263409@qq.com>
2020-06-21 21:46:18 +0800

    login

---
+++ src/page/Login/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="login-bj"><img src="../../assets/images/login_bj.png"></div>
+    <login-box></login-box>
+  </div>
+</template>
+
+<script>
+import LoginBox from '../Login/commonents/LoginBox'
+export default {
+  components:{
+    LoginBox
+  }
+}
+</script>
+
+<style scoped>
+ .login-bj{
+   width:1920px;
+   height:1080px;
+   position:fixed;
+   z-index: -9999;
+ }
+ .login-bj img{
+   width:1920px;
+   height:1080px;
+ }
+</style>
---
+++ src/page/Login/commonents/LoginBox.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="login-box">
+    <form>
+      <div class="admin">账号：<input type="text"></div>
+      <div class="password">密码：<input type="text"></div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .login-box{
+    width:300px;
+    height:300px;
+    background-color: white;
+    position: absolute;
+    left:50%;
+    top:50%;
+    transform: translate(-50%,-50%);
+    border-radius: 15px;
+    opacity: 0.6;
+  }
+  .login-box form{
+    text-align: center;
+    margin-top:50px;
+  }
+  .login-box .admin input{
+    border:1px solid red;
+  }
+  .login-box .password{
+      margin-top:30px;
+  }
+  .login-box .password input{
+    border:1px solid red;
+  }
+</style>
